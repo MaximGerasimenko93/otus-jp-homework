838ae90e20bd1f51e9b2b02e953df4605040796e
MaximGerasimenko93 <gerasimx93@gmail.com>
2023-05-11 19:53:20 +0700

    Сделал толстый jar

---
+++ hw01-gradle/src/application/HelloOtus.java
@@ -0,0 +1,11 @@
+package application;
+
+import com.google.common.base.Splitter;
+
+public class HelloOtus {
+    public static void main(String[] args) {
+        String greetings = "Hello Otus";
+        Splitter.on(" ")
+                .split(greetings);
+    }
+}
